37205f5afd5a870f3a93d72cb9363b2c2b442c7f
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-02-22 14:36:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+btn=document.querySelector("#btn")
+btn.addEventListener("mouseover",changes)
+function changes(e)
+{
+	btn.style.cssText="background-color:yellow; color:black; border-radius:30px;"
+}
